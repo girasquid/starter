31e34a465c7ca6577081309d6032dfa8d986dc49
Adam Patterson <hello@adampatterson.ca>
2014-10-29 15:02:47 -0600

    Yes

--- install.sh
+++ install.sh
@@ -2,9 +2,31 @@
 # curl -sL http://bit.ly/_goauto | sh
 echo "*** Installing..."
 
-echo " __
-/__ _  /\   _|_ _
-\_|(_)/--\|_||_(_)
+echo "
+ ____________________
+< Who you Gonna Call! >
+ --------------------
+          \
+           \
+            \          __---__
+                    _-       /--______
+               __--( /     \ )XXXXXXXXXXX\v.
+             .-XXX(   O   O  )XXXXXXXXXXXXXXX-
+            /XXX(       U     )        XXXXXXX\
+          /XXXXX(              )--_  XXXXXXXXXXX\
+         /XXXXX/ (      O     )   XXXXXX   \XXXXX\
+         XXXXX/   /            XXXXXX   \__ \XXXXX
+         XXXXXX__/          XXXXXX         \__---->
+ ---___  XXX__/          XXXXXX      \__         /
+   \-  --__/   ___/\  XXXXXX            /  ___--/=
+    \-\    ___/    XXXXXX              '--- XXXXXX
+       \-\/XXX\ XXXXXX                      /XXXXX
+         \XXXXXXXXX   \                    /XXXXX/
+          \XXXXXX      >                 _/XXXXX/
+            \XXXXX--__/              __-- XXXX/
+             -XXXXXXXX---------------  XXXXXX-
+                \XXXXXXXXXXXXXXXXXXXXXXXXXX/
+                  ""VXXXXXXXXXXXXXXXXXXV""
 
 "
 curl -sL https://github.com/goauto/srarter/tarball/master | tar -xz --strip-components 1
