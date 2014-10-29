adda0b5ccd1cfea2704f3999fec8c1cf6b25df9d
Adam Patterson <hello@adampatterson.ca>
2014-10-29 15:07:17 -0600

    Dino

--- install.sh
+++ install.sh
@@ -3,30 +3,21 @@
 echo "*** Installing..."
 
 echo "
- ____________________
-< Who you Gonna Call! >
- --------------------
-          \
-           \
-            \          __---__
-                    _-       /--______
-               __--( /     \ )XXXXXXXXXXX\v.
-             .-XXX(   O   O  )XXXXXXXXXXXXXXX-
-            /XXX(       U     )        XXXXXXX\
-          /XXXXX(              )--_  XXXXXXXXXXX\
-         /XXXXX/ (      O     )   XXXXXX   \XXXXX\
-         XXXXX/   /            XXXXXX   \__ \XXXXX
-         XXXXXX__/          XXXXXX         \__---->
- ---___  XXX__/          XXXXXX      \__         /
-   \-  --__/   ___/\  XXXXXX            /  ___--/=
-    \-\    ___/    XXXXXX              '--- XXXXXX
-       \-\/XXX\ XXXXXX                      /XXXXX
-         \XXXXXXXXX   \                    /XXXXX/
-          \XXXXXX      >                 _/XXXXX/
-            \XXXXX--__/              __-- XXXX/
-             -XXXXXXXX---------------  XXXXXX-
-                \XXXXXXXXXXXXXXXXXXXXXXXXXX/
-                  ""VXXXXXXXXXXXXXXXXXXV""
+
+                                 .       .
+                                / `.   .' \
+                        .---.  <    > <    >  .---.
+                        |    \  \ - ~ ~ - /  /    |
+                         ~-..-~             ~-..-~
+                     \~~~\.'                    `./~~~/
+           .-~~^-.    \__/                        \__/
+         .'  O    \     /               /       \  \
+        (_____,    `._.'               |         }  \/~~~/
+         `----.          /       }     |        /    \__/
+               `-.      |       /      |       /      `. ,~~|
+                   ~-.__|      /_ - ~ ^|      /- _      `..-'   f: f:
+                        |     /        |     /     ~-.     `-. _||_||_
+                        |_____|        |_____|         ~ - . _ _ _ _ _>
 
 "
 curl -sL https://github.com/goauto/srarter/tarball/master | tar -xz --strip-components 1
