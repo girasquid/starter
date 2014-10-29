2d56c33b473de1118dedb6315b2b59a6b9fb600d
Adam Patterson <hello@adampatterson.ca>
2014-10-29 14:46:04 -0600

    shell update

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # curl get.tcms.me | sh
+lsecho "*** Installing..."
 
-echo "*** Installing Tentacle CMS..."
-echo " _/_/_/_/_/ _/ _/ _/ _/_/ _/_/_/ _/_/_/_/ _/_/_/ _/_/_/ _/ _/_/ _/ _/_/_/_/ _/ _/ _/ _/ _/ _/ _/ _/_/_/_/ _/ _/ _/ _/ _/ _/ _/ _/ _/ _/ _/ _/_/_/ _/ _/ _/_/ _/_/_/ _/_/_/ _/ _/_/_/"
+echo " __
+/__ _  /\   _|_ _
+\_|(_)/--\|_||_(_)
 
-curl -sL https://github.com/adampatterson/Tentacle/tarball/beta-wip | tar -xz --strip-components 1
-echo " *** All done, View the site in your web browser to complete the installation."
+"
+curl -sL https://github.com/goauto/srarter/tarball/master | tar -xz --strip-components 1
 
-#   All done!'; #
-
-echo 'To start the install simply refresh this page to begin.';
+echo 'All done!';
