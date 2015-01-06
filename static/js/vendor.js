5726beabf9424d91ecf533f1431beb0a690cb847
Adam Patterson <hello@adampatterson.ca>
2015-01-05 21:19:55 -0700

    Updated for new Codekit config

--- static/js/vendor.js
+++ static/js/vendor.js
@@ -1,1 +1,15 @@
-
+// @codekit-prepend "../vendor/jquery/dist/jquery.js";
+// @codekit-prepend "../vendor/ga-assets/js/jquery-ui.custom/jquery-ui.min.js";
+// @codekit-prepend "../vendor/bootstrap/dist/js/bootstrap.js";
+// @codekit-prepend "../vendor/modernizr/modernizr.js";
+// @codekit-prepend "../vendor/detectizr/dist/detectizr.js";
+// @codekit-prepend "../vendor/scrollReveal.min.js";
+// @codekit-prepend "../vendor/jquery-nstslider/dist/jquery.nstSlider.min.js";
+// @codekit-prepend "../vendor/ga-assets/js/hero.videoBG.js";
+// @codekit-prepend "../vendor/happyjs/happy.js";
+// @codekit-prepend "../vendor/happyjs/happy.methods.js";
+// @codekit-prepend "../vendor/owl.carousel/dist/owl.carousel.js";
+// @codekit-prepend "../vendor/jquery-pjax/jquery.pjax.js";
+// @codekit-prepend "../vendor/uri.js/src/URI.min.js";
+// @codekit-prepend "../vendor/uri.js/src/jquery.URI.min.js";
+// @codekit-prepend "../vendor/nprogress/nprogress.js";
