121494efac0ab30667096c72c01aa1b81ed9e100
Adam Patterson <hello@adampatterson.ca>
2015-01-16 22:38:55 -0700

    starter cleanup

---
+++ static/js/app.js
@@ -0,0 +1,16 @@
+var dataTarget;
+
+var newSite = {
+    init: function (event) {
+        this.bindEvents();
+    },
+
+    // Binds the Events to the elements on the site
+    bindEvents: function () {
+
+    }
+};
+
+jQuery(document).ready(function($) {
+    newSite.init();
+});
